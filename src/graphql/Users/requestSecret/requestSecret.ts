41a503dfd5e6045f7c80e80e005d704d6b27c6ac
Kishore Newton <kishorenke@gmail.com>
2020-10-28 13:36:46 +0530

    feat: send secret finished

--- src/graphql/Users/requestSecret/requestSecret.ts
+++ src/graphql/Users/requestSecret/requestSecret.ts
@@ -1,5 +1,5 @@
 import { prisma } from '../../../../generated/prisma-client';
-import { generateSecret } from '../../../utils';
+import { generateSecret, sendSecretMail } from '../../../utils';
 
 export default {
   Mutation: {
@@ -8,6 +8,7 @@ export default {
       const loginSecret = generateSecret();
       console.log(loginSecret);
       try {
+        await sendSecretMail(email, loginSecret)
         await prisma.updateUser({
           data: { loginSecret },
           where: { email }
