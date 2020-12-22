dcd3114ac6219a2e0b0b56c5ccc4b4bc126a681c
Kishore Newton <kishorenke@gmail.com>
2020-12-23 03:55:16 +0530

    fragments

--- src/fragments.ts
+++ src/fragments.ts
@@ -1,8 +1,49 @@
+export const USER_FRAGMENT = `
+    fragment UserParts on User {
+        id
+        userename
+    }
+`;
+
 export const COMMENT_FRAGMENT = `
     fragment CommentParts on Comment {
         id
         text
         user {
+            ${USER_FRAGMENT}
+        }
+    }
+`;
+
+export const FILE_FRAGMENT = `
+    fragment FileParts on File {
+        id
+        url
+    }
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post {
+        id
+        location
+        caption
+        likes {
+            id
+        }
+        files {
+            id
+            url
+        }
+        comments {
+            id
+            text
+            user {
+                id
+                username
+            }
+        }
+        user {
+            id
             username
         }
     }
--- src/graphql/Post/seeFullPost/seeFullPost.ts
+++ src/graphql/Post/seeFullPost/seeFullPost.ts
@@ -1,21 +1,11 @@
 import { prisma } from '../../../../generated/prisma-client';
-import { COMMENT_FRAGMENT } from '../../../fragments';
+import { FULL_POST_FRAGMENT } from '../../../fragments';
 
 export default {
   Query: {
-    seeFullPost: async (_: undefined | null, args: any) => {
+    seeFullPost: (_: undefined | null, args: any) => {
       const { id } = args;
-      const post = await prisma.post({ id });
-      const comments = await prisma
-        .post({ id })
-        .comments()
-        .$fragment(COMMENT_FRAGMENT);
-      const files = await prisma.post({ id }).files();
-      return {
-        post,
-        comments,
-        files
-      };
+      return prisma.post({ id }).$fragment(FULL_POST_FRAGMENT);
     }
   }
 };
