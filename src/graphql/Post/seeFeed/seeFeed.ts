c42444f4ff71e536ee217543ac6c235b8c51639b
Kishore Newton <kishorenke@gmail.com>
2020-12-23 12:53:48 +0530

    added created at and updated at

---
+++ src/graphql/Post/seeFeed/seeFeed.ts
@@ -0,0 +1,25 @@
+import { prisma } from '../../../../generated/prisma-client';
+
+export default {
+  Query: {
+    seeFeed: async (
+      _: undefined,
+      __: undefined,
+      { req, isAuthenticated }: { req: any; isAuthenticated: any }
+    ) => {
+      isAuthenticated(req);
+      const { user } = req;
+      const following = await prisma
+        .user({ id: user.id })
+        .following();
+      return prisma.posts({
+        where: {
+          user: {
+            id_in: [...following.map(user => user.id), user.id]
+          }
+        },
+        orderBy: "caption_DESC"
+      });
+    }
+  }
+};
